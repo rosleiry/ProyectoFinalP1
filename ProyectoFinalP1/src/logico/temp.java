0bf2e5ad5d24c4205ebfee13578e54729fbdc7c9
Robert <20180840@ce.pucmm.edu.do>
2020-03-05 14:55:29 -0400

    creacion de etc

--- ProyectoFinalP1/src/logico/temp.java
+++ ProyectoFinalP1/src/logico/temp.java
@@ -2,8 +2,22 @@ package logico;
 
 public class temp {
 
-	public temp() {
-		// TODO Auto-generated constructor stub
+	
+	private String nombre;
+	private String apellido;
+	public String getNombre() {
+		return nombre;
 	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
 
 }
