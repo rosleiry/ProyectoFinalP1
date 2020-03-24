06a1ee24c56a9ac3a6914505244398d6cef4e257
rosleiry <20180627@ce.pucmm.edu.do>
2020-03-23 22:37:40 -0400

    Creacion atributos, constructor, getters y setters de la clase component

---
+++ ProyectoFinalP1/src/logico/Componente.java
@@ -0,0 +1,52 @@
+package logico;
+
+public abstract class Componente {
+
+	protected int cantDisponible;
+	protected int numSerie;
+	protected float precioComp;
+	protected String marca;
+	
+	public Componente(int cantDisponible, int numSerie, float precioComp, String marca) {
+		super();
+		this.cantDisponible = cantDisponible;
+		this.numSerie = numSerie;
+		this.precioComp = precioComp;
+		this.marca = marca;
+	}
+
+	public int getCantDisponible() {
+		return cantDisponible;
+	}
+
+	public void setCantDisponible(int cantDisponible) {
+		this.cantDisponible = cantDisponible;
+	}
+
+	public int getNumSerie() {
+		return numSerie;
+	}
+
+	public void setNumSerie(int numSerie) {
+		this.numSerie = numSerie;
+	}
+
+	public float getPrecioComp() {
+		return precioComp;
+	}
+
+	public void setPrecioComp(float precioComp) {
+		this.precioComp = precioComp;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+
+}
